6f5eddc323708ea0fe6fcd332a2e6e716bd54d21
pengmingze <mingzepeng@sina.com>
2013-05-24 21:05:12 +0800

    增加图片文件。。。

---
+++ _assets/js/index.js
@@ -0,0 +1,29 @@
+//Demo Page
+if ( $('.index').length ) {
+
+    //Navigation
+    // $('#primary-navigation').change(function(){
+    //     var _current = $(this).find('option:selected').val();
+    //     $('#' + _current + '').smoothScroll();
+    // });
+
+    //Simple Tabs Example
+
+
+        var simpleTabs = new g5Tabs();
+            simpleTabs.init({
+                el: $('#category')
+            });
+            simpleTabs.init({
+                el: $('#tasks')
+            });
+
+
+    // //Buttons Example
+    // if ( $('#buttons-example').length ) {
+    //     $('#buttons-example').find('.button').on('click', function(event){
+    //         event.preventDefault();
+    //     });
+    // }
+    
+}
