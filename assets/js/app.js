9f14542725efb038ea11bd612c61e0f13dc2a61d
pengmingze <mingzepeng@sina.com>
2013-06-07 23:19:37 +0800

    清理了一些无用的东西

--- assets/js/app.js
+++ assets/js/app.js
@@ -40,9 +40,6 @@
     //Strict within entire scope
     'use strict';
 
-    //App
-    var WebApp;
-
     //Console.log Fallback
     if ( !window.console ) { 
         window.console = function(){
@@ -56,66 +53,30 @@
         {
             load: [
                 'assets/js/libs/jquery-1.9.0.min.js'
-            ],
-            complete: function(){
-                if ( !window.jQuery ) {
-                    yepnope([
-                        {
-                            load: [
-                                'assets/js/libs/jquery-1.9.0.min.js'
-                            ],
-                            complete: function(){
-                                console.log('G5: CDN Failed - Loaded local version of jQuery.');
-                            }
-                        }
-                    ]);
-                }
-            }
+            ]
         },
         {
             load: [
-                'assets/js/libs/jquery.metadata.js',
-                'assets/js/utility.js',
+          //      'assets/js/libs/jquery.metadata.js',
+         //       'assets/js/utility.js',
                 'assets/js/tabs.js',
                 'assets/flexslider/jquery.flexslider.js'
             ],
             complete: function(){
 
-                //Loaded
-                console.log('G5: Primary JS Loaded');
-
                 //Ready
                 $(function(){
 
-                    console.log('G5: DOM Ready');
-
-                    //Load Secondary Assets & Add Functionalty
-                    //Assets that don't need to be immediately loaded and/or executed
-                    WebApp = new G5App;
-                    WebApp.init({
-                        iOsOrientationChangeFix: true,
-                        placeholderFallBack: true,
-                        customInput: true,
-                        simpleModals: true,
-                        jQueryFormValidation: true
-                    });
-
-                    (function(){
-
-                        if ( WebApp.cache.body.is('.index') ) {
-                            yepnope([
-                                {
-                                    load: [
-                                        'assets/js/index.js'
-                                    ],
-                                    complete: function(){
-                                        console.log('G5: index JS Loaded');
-                                    }
-                                }
-                            ]);
+                   yepnope([
+                        {
+                            load: [
+                                'assets/js/index.js'
+                            ],
+                            complete: function(){
+                                console.log('G5: index JS Loaded');
+                            }
                         }
-
-                    }());
+                    ]);
 
                 });
 
